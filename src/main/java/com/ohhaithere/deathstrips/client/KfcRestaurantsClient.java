3f47172804b7680ae9c12de98ccb978edc7bee7a
frostymaster <frostmasterwork@gmail.com>
2019-09-11 10:28:39 +0300

    ignore ssl certs on resttemplate

--- src/main/java/com/ohhaithere/deathstrips/client/KfcRestaurantsClient.java
+++ src/main/java/com/ohhaithere/deathstrips/client/KfcRestaurantsClient.java
@@ -2,11 +2,15 @@ package com.ohhaithere.deathstrips.client;
 
 import com.ohhaithere.deathstrips.dto.KfcRestaurantDto;
 import java.util.List;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +20,15 @@ public class KfcRestaurantsClient {
   private static final String KFC_API_URL = "https://new-api.kfc.ru/v1/restaurants";
 
   public List<KfcRestaurantDto> getRestaurants() {
-    RestTemplate restTemplate = new RestTemplate();
+    CloseableHttpClient httpClient =
+        HttpClients.custom()
+            .setSSLHostnameVerifier(new NoopHostnameVerifier())
+            .build();
+    HttpComponentsClientHttpRequestFactory reqFactory =
+        new HttpComponentsClientHttpRequestFactory();
+    reqFactory.setHttpClient(httpClient);
+
+    RestTemplate restTemplate = new RestTemplate(reqFactory);
     HttpEntity<String> request = new HttpEntity<String>("parameters", getHeaders());
     ResponseEntity<List<KfcRestaurantDto>> response = restTemplate.exchange(KFC_API_URL,
         HttpMethod.POST,
